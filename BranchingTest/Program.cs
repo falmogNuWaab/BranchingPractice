1842f563a379e1431573060f13bbc35f17db930f
HPHAN1 <huyphan@rocketmortgage.com>
2021-11-03 15:21:01 -0400

    Added line 10, moved line 9 to line 11

--- BranchingTest/Program.cs
+++ BranchingTest/Program.cs
@@ -6,8 +6,9 @@ namespace BranchingTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Jimmy World!");
+            Console.WriteLine("Let's try comething new");
+            Console.WriteLine("Hello Wii's World!");
         }
     }
 }
